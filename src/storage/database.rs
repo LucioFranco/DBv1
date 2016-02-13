ee238f61c6004589f5b63d024c5f1fb2626594ab
Lucio Franco <lucio.franco@du.edu>
2016-02-13 01:12:25 -0700

    more fixes for load; not working

--- src/storage/database.rs
+++ src/storage/database.rs
@@ -4,20 +4,20 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::fs::DirBuilder;
 
-use bincode::rustc_serialize::{encode_into};
+use bincode::rustc_serialize::{encode_into, decode_from, DecodingError};
 use bincode::SizeLimit;
 
 
 pub struct Database {
     path: String,
     metadata_buf: Option<Rc<File>>,
-    metadata: DatabaseMetadata
+     metadata: DatabaseMetadata
 }
 
 #[derive(RustcEncodable, RustcDecodable, PartialEq, Clone)]
-struct DatabaseMetadata {
+ struct DatabaseMetadata {
     name: String,
-    tables: u16
+    tables: u8
 }
 
 impl Database {
@@ -39,7 +39,7 @@ impl Database {
 
         let mut f: File = File::create(Path::new(&self.path).join(&self.metadata.name)).unwrap();
 
-        let limit = SizeLimit::Bounded(20);
+        let limit = SizeLimit::Infinite;
 
         encode_into(&Some(self.metadata.clone()), &mut f, limit).unwrap();
 
@@ -47,15 +47,20 @@ impl Database {
         info!("Added database: {}", self.metadata.name);
     }
 
-    #[allow(dead_code)]
-    pub fn load(&mut self) {
-        let mut f = File::open("/tmp/database_test".to_string() + &self.metadata.name).unwrap();
-        let mut name = String::new();
-        f.read_to_string(&mut name).unwrap();
+    #[warn(dead_code)]
+    pub fn load(&mut self) -> Result<(), DecodingError>{
+        let mut f = File::open(Path::new(&self.path).join(&self.metadata.name)).unwrap();
+        let limit = SizeLimit::Infinite;
+        let tmp: DatabaseMetadata = try!(decode_from(&mut f, limit));
 
-        if name == self.metadata.name.to_string() {
+        if tmp.name == self.metadata.name.to_string() {
             self.metadata_buf = Some(Rc::new(f));
         }
+        Ok(())
+    }
+
+    pub fn get_name(&self) -> String {
+        self.metadata.name.clone()
     }
 }
 
@@ -63,15 +68,34 @@ impl Database {
 mod test {
     use super::Database;
     use std::fs::{File, metadata};
+    use std::path::Path;
 
     #[test]
     fn create_db() {
         let path = "/tmp/test1/";
-        let mut db = Database::new(&path, "test_db");
+        let name = "test_db";
+        let mut db = Database::new(&path, &name);
         db.create();
 
         assert!(metadata(&path).is_ok());
         assert!(metadata(&path).unwrap().is_dir());
-        assert!(File::open("/tmp/test1/test_db").is_ok());
+        assert!(File::open(Path::new(&path).join(&name)).is_ok());
+    }
+
+    #[test]
+    fn load_db() {
+        #![ignore]
+        let path = "/tmp/test1/";
+        let name = "test_db2";
+        let mut db = Database::new(&path, &name);
+        db.create();
+
+        let mut db2 = Database::new(&path, &name);
+        if let Err(e) = db2.load() {
+            println!("{:?}", e);
+            assert!(false);
+        }
+
+        assert_eq!(db.get_name(), db2.get_name());
     }
 }
