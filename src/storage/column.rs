2a1e9de0189ded758c578d1ed2e0f7b6cdd99546
Lucio Franco <lucio.franco@du.edu>
2016-02-24 23:28:59 -0700

    added types to columns

--- src/storage/column.rs
+++ src/storage/column.rs
@@ -1,1 +1,15 @@
-pub struct Column;
+use super::types::Types;
+
+pub struct Column {
+    col_type: Types
+}
+
+impl Column {
+    pub fn new(c_type: Types) -> Self {
+        Column { col_type: c_type }
+    }
+
+    pub fn size(&self) -> u32 {
+        self.col_type.size()
+    }
+}
