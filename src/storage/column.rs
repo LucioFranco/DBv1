0f17139e6fe5a3ff158b8fcb87415d97d2bddb55
Lucio Franco <lucio.franco@du.edu>
2016-02-27 22:02:24 -0700

    added column and table

--- src/storage/column.rs
+++ src/storage/column.rs
@@ -1,15 +1,23 @@
 use super::types::Types;
 
 pub struct Column {
+    name: String,
     col_type: Types
 }
 
 impl Column {
-    pub fn new(c_type: Types) -> Self {
-        Column { col_type: c_type }
+    pub fn new(name: &str, c_type: Types) -> Self {
+        Column {
+            name: name.to_string(),
+            col_type: c_type
+        }
     }
 
     pub fn size(&self) -> u32 {
         self.col_type.size()
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name.clone()
     }
 }
