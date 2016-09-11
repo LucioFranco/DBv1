2f5fc3a91dc1774eaa045a62442865b5ac2e7c42
Lucio Franco <lucio.franco@du.edu>
2016-09-10 22:52:04 -0400

    WIP

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,15 +1,30 @@
 //! the network module for the database
-use std::net::{TcpListener, ToSocketAddrs};
+use std::net::{TcpListener, TcpStream, ToSocketAddrs};
+use std::thread;
+
 use error::Result;
 
-struct Server {
+pub struct Server {
     listener: TcpListener
 }
 
 impl Server {
-    fn new<A: ToSocketAddrs>(addrs: A) -> Result<Server> {
+    pub fn new<A: ToSocketAddrs>(addrs: A) -> Result<Server> {
         Ok(Server {
             listener: try!(TcpListener::bind(addrs))
         })
+    }
+
+    pub fn handle<F>(&mut self, f: F) -> Result<()>
+        where F: FnOnce(TcpStream) -> Result<()> {
+        for stream in self.listener.incoming() {
+            let stream = try!(stream);
+
+            thread::spawn(move || {
+               f(stream);
+            });
+        }
+
+        Ok(())
     }
 }
